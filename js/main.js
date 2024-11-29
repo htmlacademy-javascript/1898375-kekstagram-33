b04d3ce3ccdcdc2991b936597989f4f4b81c1ab3
Vitaliy Vasiliev <93582413+Ierixorn@users.noreply.github.com>
2024-11-29 14:02:29 +0300

    Добавление коментариев

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ import './util.js';
 import './photo-comments.js';
 import {createDescriptionArray} from './photo-discriptions.js';
 import {renderPhotos} from './photo-rendering.js';
-import './photo-detail-view.js';
+import {showPost} from './photo-detail-view.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const similarPost = createDescriptionArray();
 
 renderPhotos(picturesContainer, similarPost);
+
+console.log(similarPost[0]);
+
+showPost(similarPost[0]);
--- js/photo-detail-view.js
+++ js/photo-detail-view.js
@@ -1,3 +1,41 @@
 const picturesContainer = document.querySelector('.pictures');
-const photoIcons = picturesContainer.querySelectorAll('.pictures');
-const bigPicture = document.querySelector('.big-picture');
+const post = document.querySelector('.big-picture');
+const closeButton = post.querySelector ('.big-picture__cancel');
+
+const closePost = () => {
+  closeButton.addEventListener ('click', () => {
+    post.classList.add('hidden');
+  });
+
+  document.addEventListener ('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      post.classList.add('hidden');
+    }
+  });
+};
+
+const hideCommentsCount = () => {
+  post.querySelector('.social__comment-count').classList.add('hidden');
+  post.querySelector('.comments-loader').classList.add('hidden');
+};
+
+const fillPost = (postPrevie) => {
+  post.querySelector('.big-picture__img').children[0].src = postPrevie.url;
+  post.querySelector('.likes-count').textContent = postPrevie.likes;
+  post.querySelector('.social__comment-total-count').textContent = postPrevie.comments.length;
+  post.querySelector('.social__caption').textContent = postPrevie.description;
+
+};
+
+
+const showPost = (postPrevie) => {
+  closePost();
+  fillPost(postPrevie);
+  hideCommentsCount();
+  picturesContainer.addEventListener('click', () => {
+    post.classList.remove('hidden');
+  });
+};
+
+
+export {showPost};
